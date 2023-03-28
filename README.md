8aa53c47149c0e3d9e47f3afd34098f366eee082
João Silva <108815251+jptsantossilva@users.noreply.github.com>
2023-03-28 14:28:48 +0100

    intitial

---
+++ README.md
@@ -0,0 +1,21 @@
+# SP500 Top Performers
+
+Get SP500 best and worst performers from the last 1-24 months
+
+## Installation
+
+- Python Libraries
+```text
+pip install streamlit
+pip install yfinance
+```
+
+
+## Disclaimer
+This software is for educational purposes only. Use the software at **your own risk**. The authors and all affiliates assume **no responsibility for your trading results**. **Do not risk money that you are afraid to lose**. There might be **bugs** in the code. This software does not come with **any warranty**.
+
+## 📝 License
+
+This project is [MIT](https://github.com/jptsantossilva/sp500-top-performers/blob/main/LICENSE.md) licensed.
+
+Copyright © 2023 [João Silva](https://github.com/jptsantossilva)
---
+++ main.py
@@ -0,0 +1,51 @@
+# %%
+import yfinance as yf
+import pandas as pd
+import streamlit as st
+from pandas.tseries.offsets import DateOffset
+
+# %%
+tickers = pd.read_html('https://en.wikipedia.org/wiki/List_of_S&P_500_companies')[0].Symbol
+
+# %%
+tickers = tickers.to_list()
+
+# %%
+# tickers
+
+# %%
+@st.cache_data
+def get_data():
+    df = yf.download(tickers, start = '2020-01-01')
+    df = df['Close']
+    return df
+
+df = get_data()
+
+st.title('SP500 Top performance')
+
+n = st.number_input('Performance horizon (months)', min_value=1, max_value=24)
+
+def get_ret(df, n):
+    previous_prices = df[:df.index[-1] - DateOffset(months=n)].tail(1).squeeze()
+    recent_prices = df.loc[df.index[-1]]
+    ret_df = (recent_prices/previous_prices - 1)*100
+    return previous_prices.name, ret_df
+
+date, ret_df = get_ret(df,n)
+
+winners, losers = ret_df.nlargest(10), ret_df.nsmallest(10)
+
+winners.name, losers.name = 'winners', 'losers' 
+
+st.table(winners)
+st.table(losers)
+
+
+winner_pick = st.selectbox('Pick a winner:', winners.index)
+st.line_chart(df[winner_pick][date:])
+
+loser_pick = st.selectbox('Pick a loser:', losers.index)
+st.line_chart(df[loser_pick][date:])
+
+
